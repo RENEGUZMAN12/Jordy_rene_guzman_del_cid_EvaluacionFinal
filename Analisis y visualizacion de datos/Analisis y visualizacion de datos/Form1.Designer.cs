3d49f0d7c3b33f9e00e90fd7c06824ba86757504
RENEGUZMAN12 <125596267+RENEGUZMAN12@users.noreply.github.com>
2023-07-24 09:47:10 -0600

    Add files via upload

---
+++ Analisis y visualizacion de datos/Analisis y visualizacion de datos/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace Analisis_y_visualizacion_de_datos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.txt_nombre_datos = new System.Windows.Forms.TextBox();
+            this.btn_añadir_datos_dtg = new System.Windows.Forms.Button();
+            this.chart_graficacion = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.DTG_tabla_datos = new System.Windows.Forms.DataGridView();
+            this.txt_cifra_dato = new System.Windows.Forms.TextBox();
+            this.btn_graficar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_graficacion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DTG_tabla_datos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_nombre_datos
+            // 
+            this.txt_nombre_datos.Location = new System.Drawing.Point(87, 131);
+            this.txt_nombre_datos.Name = "txt_nombre_datos";
+            this.txt_nombre_datos.Size = new System.Drawing.Size(100, 26);
+            this.txt_nombre_datos.TabIndex = 0;
+            // 
+            // btn_añadir_datos_dtg
+            // 
+            this.btn_añadir_datos_dtg.Location = new System.Drawing.Point(191, 184);
+            this.btn_añadir_datos_dtg.Name = "btn_añadir_datos_dtg";
+            this.btn_añadir_datos_dtg.Size = new System.Drawing.Size(107, 38);
+            this.btn_añadir_datos_dtg.TabIndex = 1;
+            this.btn_añadir_datos_dtg.Text = "Añadir datos";
+            this.btn_añadir_datos_dtg.UseVisualStyleBackColor = true;
+            this.btn_añadir_datos_dtg.Click += new System.EventHandler(this.btn_añadir_datos_dtg_Click);
+            // 
+            // chart_graficacion
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart_graficacion.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart_graficacion.Legends.Add(legend3);
+            this.chart_graficacion.Location = new System.Drawing.Point(591, 131);
+            this.chart_graficacion.Name = "chart_graficacion";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart_graficacion.Series.Add(series3);
+            this.chart_graficacion.Size = new System.Drawing.Size(483, 390);
+            this.chart_graficacion.TabIndex = 2;
+            this.chart_graficacion.Text = "chart1";
+            // 
+            // DTG_tabla_datos
+            // 
+            this.DTG_tabla_datos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DTG_tabla_datos.Location = new System.Drawing.Point(27, 228);
+            this.DTG_tabla_datos.Name = "DTG_tabla_datos";
+            this.DTG_tabla_datos.RowHeadersWidth = 62;
+            this.DTG_tabla_datos.RowTemplate.Height = 28;
+            this.DTG_tabla_datos.Size = new System.Drawing.Size(430, 293);
+            this.DTG_tabla_datos.TabIndex = 3;
+            // 
+            // txt_cifra_dato
+            // 
+            this.txt_cifra_dato.Location = new System.Drawing.Point(297, 131);
+            this.txt_cifra_dato.Name = "txt_cifra_dato";
+            this.txt_cifra_dato.Size = new System.Drawing.Size(100, 26);
+            this.txt_cifra_dato.TabIndex = 4;
+            // 
+            // btn_graficar
+            // 
+            this.btn_graficar.Location = new System.Drawing.Point(784, 87);
+            this.btn_graficar.Name = "btn_graficar";
+            this.btn_graficar.Size = new System.Drawing.Size(140, 38);
+            this.btn_graficar.TabIndex = 5;
+            this.btn_graficar.Text = "Graficar datos";
+            this.btn_graficar.UseVisualStyleBackColor = true;
+            this.btn_graficar.Click += new System.EventHandler(this.btn_graficar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1035, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 61);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre dato";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Cifra del dato";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1132, 612);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_graficar);
+            this.Controls.Add(this.txt_cifra_dato);
+            this.Controls.Add(this.DTG_tabla_datos);
+            this.Controls.Add(this.chart_graficacion);
+            this.Controls.Add(this.btn_añadir_datos_dtg);
+            this.Controls.Add(this.txt_nombre_datos);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart_graficacion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DTG_tabla_datos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_nombre_datos;
+        private System.Windows.Forms.Button btn_añadir_datos_dtg;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_graficacion;
+        private System.Windows.Forms.DataGridView DTG_tabla_datos;
+        private System.Windows.Forms.TextBox txt_cifra_dato;
+        private System.Windows.Forms.Button btn_graficar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Analisis y visualizacion de datos/Analisis y visualizacion de datos/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace Analisis_y_visualizacion_de_datos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DTG_tabla_datos.Columns.Add("columnaCategoria", "categorias");
+            DTG_tabla_datos.Columns.Add("columnaCifra", "cantidad");
+        }
+
+        private void btn_añadir_datos_dtg_Click(object sender, EventArgs e)
+        {
+            string nombre_dato = txt_nombre_datos.Text;
+            if (!string.IsNullOrEmpty(nombre_dato))
+            {
+                if (int.TryParse(txt_cifra_dato.Text,out int cantidad))
+                {
+                    // Asegúrate de que cantidad sea un índice válido dentro del rango de filas del DataGridView
+                    if (cantidad >= 0 && cantidad < DTG_tabla_datos.Rows.Count)
+                    {
+                        // Si ya existe una fila en el índice especificado, puedes actualizar sus valores
+                        DataGridViewRow fila = DTG_tabla_datos.Rows[cantidad];
+                        int columnaCategoria = 0;
+                        int columnaCifra = 1;
+                        fila.Cells[columnaCategoria].Value = nombre_dato;
+                        fila.Cells[columnaCifra].Value = cantidad;
+                    }
+                    else
+                    {
+                        // Si el índice está fuera del rango actual, agrega una nueva fila
+                        int columnaCategoria = 0;
+                        int columnaCifra = 1;
+                        DTG_tabla_datos.Rows.Add(nombre_dato, cantidad);
+                    }
+
+                }
+            }
+        }
+
+        private void btn_graficar_Click(object sender, EventArgs e)
+        {
+
+            chart_graficacion.Series.Clear();
+
+            Series serieCifras = new Series("Cifras");
+
+            serieCifras.ChartType = SeriesChartType.Column;
+
+            foreach (DataGridViewRow fila in DTG_tabla_datos.Rows)
+            {
+                // Verificamos si la fila tiene celdas y si las celdas de categoría y cifra contienen datos válidos
+                if (fila.Cells[0].Value != null && fila.Cells[1].Value != null)
+                {
+                    string categoria = fila.Cells[0].Value.ToString();
+                    int cifra = Convert.ToInt32(fila.Cells[1].Value);
+
+                    // Agregamos los datos a la serie del gráfico
+                    serieCifras.Points.AddXY(categoria, cifra);
+                }
+            }
+            serieCifras.ChartType = SeriesChartType.Pie;
+            // Agregamos la serie al Chart
+            chart_graficacion.Series.Add(serieCifras);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
